dec8426c7942e006b15de3184bca0f4229092492
Monika <mkidawska@student.agh.edu.pl>
2024-01-20 22:09:31 +0100

    change fetch url

--- src/components/utils/fetchUrl.ts
+++ src/components/utils/fetchUrl.ts
@@ -1,2 +1,2 @@
 // export const FETCH_URL = "http://localhost:8080/api/";
-export const FETCH_URL = "https://zti-project-396715.ew.r.appspot.com/api/";
+export const FETCH_URL = "https://asa-ls4bjeuhtpcma-standard-cinema-app.azuremicroservices.io/api/";
